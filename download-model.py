fb3e2a860eb37c508fad084d4248f2b116e9c0d2
Viswateja Rayapaneni <viswatejaster@gmail.com>
2024-02-19 19:39:39 +0530

    Update download-model.py

--- download-model.py
+++ download-model.py
@@ -1,31 +1,53 @@
 import json
 import boto3
 import os
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+s3 = boto3.client("s3")
+
+
 def download_s3():
-    print("--------Download")
     # Replace with your own values
-    bucket_name = "siri-model-registry"
-    download_path = os.getcwd()  # Local path to save the downloaded file
-    print(os.getcwd())
-    
-    # Create an S3 client
-    s3 = boto3.client("s3")
-    
-    # Download the file
+    download_path = os.getcwd()
+    s3_location = get_s3_location
+    s3_path = s3_location.replace("s3://", "")
+    bucket, key = s3_path.split("/", 1)
+    print("Bucket:", bucket)
+    print("Key:", key)
     try:
-        s3.download_file(bucket_name, file_key, download_path)
+        s3.download_file(bucket, key, download_path)
         print("File downloaded successfully!")
     except Exception as e:
         print(f"Error downloading file: {e}")
 
 
+def get_s3_location():
+    # Open a file in read mode
+    file_path = "config.json"
+    with open(file_path, "r") as file:
+        contents = file.read()
+
+    table_name = os.environ['table_name']
+    table = dynamodb.Table(table_name)
+
+    response = table.get_item(
+        Key={"registry-name": contents['registry-name']})
+
+    for version in response['Item']['versions']:
+        if version['version'] == Decimal(contents['version']):
+            s3_location = version['s3_location']
+    return s3_location
+
+
 def lambda_handler():
     # TODO implement
-    _=download_s3()
+    _ = download_s3()
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
 
 if __name__ == "__main__":
     lambda_handler()
