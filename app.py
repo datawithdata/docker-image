73ab691828d700d5457296d8102a66547c636f51
Viswateja Rayapaneni <viswatejaster@gmail.com>
2024-03-11 17:52:44 +0530

    updated predict and app

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ Created on Fri Dec  9 19:35:22 2022
 @author: viswateja
 """
 
-from flask import Flask,request
-from predict import input_req
+from flask import Flask, request
+from predit import input_req
 app = Flask(__name__)
 
-@app.route('/predict',methods=['POST', 'GET'])
-def predict():
-    response=input_req(request.get_json())
-    return "success"
 
-@app.route('/health',methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    response = input_req(request.get_json())
+    return response
+
+
+@app.route('/health', methods=['POST', 'GET'])
 def health():
     return "Success"
 
+
 # main driver function
 if __name__ == '__main__':
- 
-	app.run(debug=True)
+
+    app.run(debug=True)
--- predit.py
+++ predit.py
@@ -5,22 +5,26 @@ Created on Fri Dec  9 19:29:23 2022
 @author: viswateja
 """
 
-import pickle
+import joblib
+import json
+import ast
+
 file_path = "config.json"
 with open(file_path, "r") as file:
     contents = json.loads(file.read())
 
+
 def load_model():
-    loaded_model = pickle.load(open(contents['model_name'], 'rb'))
+    loaded_model = joblib.load(open(contents['model_name'], 'rb'))
     return loaded_model
 
-def predict(loaded_model,data):
+
+def predict(loaded_model, data):
     result = loaded_model.predict(data,)
     return result
 
+
 def input_req(data):
     model = load_model()
-    response = predict(model,data)
-    return {"flower_names":str(response[0])}
-
-
+    response = predict(model, ast.literal_eval(data['data']))
+    return {"flower_names": str(response[0])}
